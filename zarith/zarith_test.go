cb37ffdbe97b57e2e2827b3b232eb86bd8e0b714
Gavi Galloway <gsgalloway@users.noreply.github.com>
2019-10-21 22:52:27 -0700

    Add support for zarith signed ints (#13)
    * Add support for zarith signed ints
    
    * fix mistake in method docstring
    
    * remove unused error return value

--- zarith/zarith_test.go
+++ zarith/zarith_test.go
@@ -35,6 +35,15 @@ func TestEncode(t *testing.T) {
 	}, {
 		input:    "10100",
 		expected: "f44e",
+	}, {
+		input:    "7",
+		expected: "07",
+	}, {
+		input:    "32",
+		expected: "20",
+	}, {
+		input:    "4096",
+		expected: "8020",
 	}}
 
 	for _, testCase := range testCases {
@@ -43,6 +52,40 @@ func TestEncode(t *testing.T) {
 		require.True(ok)
 		observed, err := zarith.EncodeToHex(input)
 		require.NoError(err)
+		require.Equal(testCase.expected, observed, "mismatch for input %s", testCase.input)
+	}
+}
+
+func TestEncodeSigned(t *testing.T) {
+	require := require.New(t)
+	testCases := []zarithTestCase{{
+		input:    "0",
+		expected: "00",
+	}, {
+		input:    "-64",
+		expected: "c001",
+	}, {
+		input:    "-120053",
+		expected: "f5d30e",
+	}, {
+		input:    "30268635200",
+		expected: "80e1b5c2e101",
+	}, {
+		input:    "610913435200",
+		expected: "80f9b9d4c723",
+	}, {
+		input:    "1",
+		expected: "01",
+	}, {
+		input:    "138",
+		expected: "8a02",
+	}}
+
+	for _, testCase := range testCases {
+		input := new(big.Int)
+		_, ok := input.SetString(testCase.input, 10)
+		require.True(ok)
+		observed := zarith.EncodeSignedToHex(input)
 		require.Equal(testCase.expected, observed, "mismatch for input %s", testCase.input)
 	}
 }
@@ -71,6 +114,39 @@ func TestDecode(t *testing.T) {
 
 	for _, testCase := range testCases {
 		observedDecimal, err := zarith.DecodeHex(testCase.input)
+		require.NoError(err)
+		observed := observedDecimal.String()
+		require.Equal(testCase.expected, observed, "mismatch for input %s", testCase.input)
+	}
+}
+
+func TestDecodeSigned(t *testing.T) {
+	require := require.New(t)
+	testCases := []zarithTestCase{{
+		input:    "00",
+		expected: "0",
+	}, {
+		input:    "c001",
+		expected: "-64",
+	}, {
+		input:    "f5d30e",
+		expected: "-120053",
+	}, {
+		input:    "80e1b5c2e101",
+		expected: "30268635200",
+	}, {
+		input:    "80f9b9d4c723",
+		expected: "610913435200",
+	}, {
+		input:    "01",
+		expected: "1",
+	}, {
+		input:    "8a02",
+		expected: "138",
+	}}
+
+	for _, testCase := range testCases {
+		observedDecimal, err := zarith.DecodeSignedHex(testCase.input)
 		require.NoError(err)
 		observed := observedDecimal.String()
 		require.Equal(testCase.expected, observed, "mismatch for input %s", testCase.input)
@@ -106,7 +182,17 @@ func TestReadNext(t *testing.T) {
 	require.Error(err)
 }
 
-func TestNegative(t *testing.T) {
+func TestReadNextSigned(t *testing.T) {
+	require := require.New(t)
+	input, err := hex.DecodeString("f5d30e000000000000")
+	require.NoError(err)
+	decoded, bytesRead, err := zarith.ReadNextSigned(input)
+	require.NoError(err)
+	require.Equal(3, bytesRead)
+	require.Equal("-120053", decoded.String())
+}
+
+func TestNegativeInputForUnsignedZarithValue(t *testing.T) {
 	require := require.New(t)
 	input := big.NewInt(-10)
 	_, err := zarith.Encode(input)
